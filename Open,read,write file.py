f6141bda6b57479329cafeba19e6a6b5958febd0
TannyKorry <tannykorry@gmail.com>
2022-09-26 22:02:32 +0300

    Solution of task 1

--- Open,read,write file.py
+++ Open,read,write file.py
@@ -1,16 +1,16 @@
-# This is a sample Python script.
+with open('text.txt', 'rt', encoding='utf8') as f:
+    cook_book = {}
+    for line in f:
+        dish = line.strip()
+        recipe = []
+        count_ing = f.readline()
+        for i in range(int(count_ing)):
+            ings = f.readline()
+            name, q, unit = ings.split(' | ')
+            ingredient = {'ingredients_name': name, 'quantity': int(q), 'measure': unit.strip()}
+            recipe.append(ingredient)
+            cook_book[dish] = recipe
+        f.readline()
+import pprint
+pprint.pprint(cook_book, width=100, sort_dicts=False)
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
