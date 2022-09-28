4e52de78924ed8169acc633b4c2073dd9f5047fc
TannyKorry <tannykorry@gmail.com>
2022-09-28 21:22:17 +0300

    Basic logic of task 2

--- Open,read,write file.py
+++ Open,read,write file.py
@@ -1,3 +1,8 @@
+
+import pprint
+import json
+print('\nTask #1\n')
+
 with open('text.txt', 'rt', encoding='utf8') as f:
     cook_book = {}
     for line in f:
@@ -11,6 +16,28 @@ with open('text.txt', 'rt', encoding='utf8') as f:
             recipe.append(ingredient)
             cook_book[dish] = recipe
         f.readline()
-import pprint
-pprint.pprint(cook_book, width=100, sort_dicts=False)
+
+pprint.pprint(cook_book, width=100, sort_dicts=False) # Так красиво, как в задании не получается. Это выглядит компактнее.
+# print(json.dumps(cook_book, indent=1, ensure_ascii=False, sort_keys=False))
+
+print('\nTask #2\n')
+
+def get_shop_list_by_dishes(dishes, person_count):
+    new_dict ={}
+    for i, list in cook_book.items():# получаем список словарей [{'ingredients_name': 'Яйцо', 'quantity': 2, 'measure': 'шт'}] и т.д.
+        if i in dishes:
+            for ings_dict in list: # получаем словарь {'ingredients_name': 'Яйцо', 'quantity': 2, 'measure': 'шт'} и т.д.
+                ings = ings_dict.get('ingredients_name') # получаем назввание ингридиента, который потом сделает ключем нового словаря
+                del(ings_dict['ingredients_name']) # удаляем из словаря пару ключ-значение (название ингридиента)
+                for k, v in ings_dict.items():
+                    if k == 'quantity':
+                        ings_dict['quantity'] = v * person_count
+                new_dict[ings] = ings_dict  # формируем новый словарь
+    return new_dict
+
+
+res = get_shop_list_by_dishes(['Запеченный картофель','Омлет'], 2)
+pprint.pprint(res, width=100)
+
+
 
