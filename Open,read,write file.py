1f314ea5d4b0e1abda1f6ad48506c0415e88c6fe
TannyKorry <tannykorry@gmail.com>
2022-09-28 21:22:17 +0300

    Basic logic of task 2

--- Open,read,write file.py
+++ Open,read,write file.py
@@ -11,6 +11,27 @@ with open('text.txt', 'rt', encoding='utf8') as f:
             recipe.append(ingredient)
             cook_book[dish] = recipe
         f.readline()
+
 import pprint
-pprint.pprint(cook_book, width=100, sort_dicts=False)
+# pprint.pprint(cook_book, width=100, sort_dicts=False)
+
+
+def get_shop_list_by_dishes(dishes, person_count):
+    new_dict ={}
+    for list in cook_book.values():# получаем список словарей [{'ingredients_name': 'Яйцо', 'quantity': 2, 'measure': 'шт'}] и т.д.
+        for ings_dict in list: # получаем словарь {'ingredients_name': 'Яйцо', 'quantity': 2, 'measure': 'шт'} и т.д.
+            ings = ings_dict.get('ingredients_name') # получаем назввание ингридиента, который потом сделает ключем нового словаря
+            del(ings_dict['ingredients_name']) # удаляем словаря пару ключ-значение (название ингридиента)
+            for k, v in ings_dict.items():
+                if k == 'quantity':
+                    ings_dict['quantity'] = v * person_count
+            new_dict[ings] = ings_dict  # формируем новый словарь
+    return new_dict
+
+
+res = get_shop_list_by_dishes(cook_book, 5)
+
+pprint.pprint(res, width=100, sort_dicts=False)
+
+
 
