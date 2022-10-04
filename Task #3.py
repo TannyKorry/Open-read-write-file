d80ac294cb24fc92d9fe4132b0c648be35b64c7b
TannyKorry <tannykorry@gmail.com>
2022-10-04 20:58:17 +0300

    Recommend

--- Task #3.py
+++ Task #3.py
@@ -38,3 +38,33 @@ with open('final_text.txt', 'w', encoding='utf8') as final:
         if q_str == max(dict_basic.keys()):
             for file_name, text in v.items():
                 final.writelines([str(file_name), '\n', str(q_str), '\n', str(text), '\n'])
+
+
+# РЕКОМЕНДАЦИЯ ПРЕПОДАВАТЕЛЯ
+# ________________________________________________
+# Татьяна, добрый день.
+# Спасибо за работу.
+# Задача 3 - предложу вот такой подход к решению
+#
+#
+ # import os
+#
+# def compile_files(files_list):
+#     data = {}
+#     for file in files_list:
+#         with open(file, encoding="utf-8") as f:
+#             file_data = f.readlines()
+#             data[len(file_data)] = (file, " ".join(file_data))
+#
+#     data = dict(sorted(data.items()))
+#
+#     with open("result_data.txt", "w", encoding="utf-8") as new_file:
+#         for key, value in data.items():
+#             new_file.write(f"{value[0]} \n")
+#             new_file.write(f"{key} \n")
+#             new_file.write(f"{value[1]} \n")
+#
+#
+# files = ["1.txt", "2.txt", "3.txt"]
+# files = [os.path.join(os.getcwd(), file) for file in files]
+# compile_files(files)
